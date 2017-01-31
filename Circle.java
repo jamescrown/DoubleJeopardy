657e26a9b171094524671f88d4cac87debe2e7a1
clodaghlalor <clodagh.lalor@ucdconnect.ie>
2017-01-31 14:16:29 +0000

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,22 +1,24 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Graphics;
-import java.awt.color.*;
-//lmao x3
+import java.awt.Color;
 
 public class Circle extends JPanel{
 	
 	public void paint(Graphics g){
 		
-		setSize(500,500); //width and height of JPAnel
-		g.drawOval(100, 100, 50, 50); //draws at these coordinates a circle of height and width 50
+		setSize(1000,600); //width and height of JPAnel
+		g.drawOval(920, 500, 50, 50); //draws at these coordinates a circle of height and width 50
+		
+		//g.fillOval(100, 100, 50, 50); //color
 		
 	}
 
 	public static void main(String [] args){
 		
 		JFrame GameBoard = new JFrame();
-		GameBoard.setSize(600, 600);
+		GameBoard.setSize(1000, 600);
+		//meBoard.setIconImage();
 		
 		Circle CirclePanel = new Circle();
 		
