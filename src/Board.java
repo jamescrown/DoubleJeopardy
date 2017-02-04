1f2fdc450bb6f143f69ab9dccafd1d0ec096b8ff
jamescrown <jamescrown12345@gmail.com>
2017-02-04 12:52:36 +0000

    Add files via upload
    its the board.java file thats the important one , sorry for late

--- src/Board.java
+++ src/Board.java
@@ -1,161 +1,142 @@
-import java.awt.*;
-import java.util.Random;
-
 import javax.swing.*;
 
-public class Board extends JFrame {
-	
-	JButton b1;
-	JLabel l1;
-	JLabel l5;
-	JPanel p1;
-	JPanel p2;
-	JPanel player1;
-	JPanel player2;
-	JPanel player3;
-	JLabel pll1;
-	JLabel pll2;
-	JLabel pll3;
-	JFrame f;
+import java.awt.*;
+import java.awt.geom.*;
+//first attempt
 
-		public Board()
-		{
-		
-		f = new JFrame();
-		f.setTitle("Monopoly Board");
-		f.setSize(800,800);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setVisible(true);
-	
-		f.setLayout(null);
-		f.setContentPane(new JLabel(new ImageIcon("MonopolyBoard.jpg")));
-		f.setSize(799,799);
-		f.setSize(800,800);
-//		
-//		p1 = new JPanel();
-//		p1.setBackground(Color.GRAY);
-		
-		p2 = new JPanel();
-		p2.setBackground(Color.PINK);
-		
-		//player1
-		player1 = new JPanel();
-		player1.setBackground(Color.YELLOW);
-		player1.setLocation(700, 710);
-		player1.setSize(18, 18);
-		pll1 = new JLabel("P1");
-		player1.add(pll1);
+class Board extends JFrame{
 
-		//player2
-		player2 = new JPanel();
-		player2.setBackground(Color.RED);
-		player2.setLocation(700, 690);
-		player2.setSize(18, 18);
-		pll2 = new JLabel("P2");
-		player2.add(pll2);
-		
-		//player3
-		player3 = new JPanel();
-		player3.setBackground(Color.PINK);
-		player3.setLocation(700, 670);
-		player3.setSize(18, 18);
-		pll3 = new JLabel("P3");
-		player3.add(pll3);
-				
-//		l1 = new JLabel("This is our information panel");
-//		p1.add(l1);
-		
-		Random rand = new Random();
-		int  n = rand.nextInt(5) + 1;
-		l5 = new JLabel("I rolled a " + String.valueOf(n));
-		p2.add(l5);
-		
-//		f.add(p1);
-		f.add(p2);
-		f.add(player1);
-		f.add(player2);
-		f.add(player3);
-//		p1.setLocation(300, 400);
-//		p1.setSize(200, 100);
-		p2.setLocation(300,400);
-		p2.setSize(100, 100);
-		}
-//		 private JLabel labelPlayer1 = new JLabel(" Player 1: ");
-//		    private JLabel labelPlayer2 = new JLabel(" Player 2: ");
-//		    private JLabel labelPlayer3 = new JLabel(" Player 3: ");
-//		    
-//		    private JTextField textPlayer1 = new JTextField(25);
-//		    private JTextField textPlayer2 = new JTextField(25);
-//		    private JTextField textPlayer3 = new JTextField(25);
-//		    
-//		    private JButton buttonStart = new JButton("Start Game");
-//		     
-//		    public void Panel() {
-//		        
-//		         
-//		        // create a new panel with GridBagLayout manager
-//		        JPanel newPanel = new JPanel(new GridBagLayout());
-//		         
-//		        GridBagConstraints constraints = new GridBagConstraints();
-//		        constraints.anchor = GridBagConstraints.WEST;
-//		        constraints.insets = new Insets(10, 10, 10, 10);
-//		         
-//		        // add components to the panel
-//		        constraints.gridx = 0;
-//		        constraints.gridy = 0;     
-//		        newPanel.add(labelPlayer1, constraints);
-//		 
-//		        
-//		        constraints.gridx = 1;
-//		        newPanel.add(textPlayer1, constraints);
-//		        
-//		        constraints.gridx = 0;
-//		        constraints.gridy = 1;     
-//		        newPanel.add(labelPlayer2, constraints);
-//		       
-//		        constraints.gridx = 1;
-//		        newPanel.add(textPlayer2, constraints);
-//		        
-//		        constraints.gridx = 0;
-//		        constraints.gridy = 2;     
-//		        newPanel.add(labelPlayer3, constraints);
-//		        
-//
-//		        constraints.gridx = 1;
-//		        newPanel.add(textPlayer3, constraints);
-//		        
-//		        constraints.gridx = 0;
-//		        constraints.gridy = 3;
-//		        constraints.gridwidth = 3;
-//		        constraints.anchor = GridBagConstraints.CENTER;
-//		        
-//		        newPanel.add(buttonStart, constraints);
-//		         
-//		        // set border for the panel
-//		        newPanel.setBorder(BorderFactory.createTitledBorder(
-//		                BorderFactory.createEtchedBorder(), "Welcome to Monopoly"));
-//		         
-//		        // add the panel to this frame
-//		        add(newPanel);
-//		         
-//		        pack();
-//		        setLocationRelativeTo(null);
-//		    }
-		public static void main(String args[])
-		{
-//			 try {
-//		            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//		        } catch (Exception ex) {
-//		            ex.printStackTrace();
-//		        }
-//		         
-//		        SwingUtilities.invokeLater(new Runnable() {
-//		            @Override
-//		            public void run() {
-//		                new Panel().setVisible(true);
-//		            }
-//		        });
-			new Board();
-		}
-	}
-  
+    public Board(){
+    	
+    
+    	JPanel panel=new JPanel();
+   	 getContentPane().add(panel);
+        setSize(770, 660);
+   
+//        JFrame frame = new JFrame();
+//    	frame.setSize(1680, 1080);
+//     frame.add(panel, BorderLayout.CENTER);
+//        frame.setLayout(new BorderLayout());
+//    	frame.setVisible(true);  
+//       
+       
+        //panel.setExtendedState(JPanel.MAXIMIZED_BOTH); 
+      
+       /* JLabel title = new JLabel("monopoly");
+        getContentPane().add(title);
+        title setVisible(true);
+        */
+   
+        //  getContentPane().add(panel);
+       // JButton button =new JButton("press");
+       // panel.add(button);
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        Line2D lin = new Line2D.Float(0, 600, 770, 600);
+        Line2D lin1 = new Line2D.Float(0, 60, 770, 60);
+        Line2D lin2 = new Line2D.Float(70, 0, 70, 660);
+        Line2D lin3 = new Line2D.Float(700, 0, 700, 660);
+
+        g2.draw(lin);
+        g2.draw(lin1);
+        g2.draw(lin2);
+        g2.draw(lin3);
+
+        
+        Line2D linBOT0 = new Line2D.Float(140, 660, 140, 600);
+        Line2D linBOT1 = new Line2D.Float(210, 660, 210, 600);
+        Line2D linBOT2 = new Line2D.Float(280, 660, 280, 600);
+        Line2D linBOT3 = new Line2D.Float(350, 660, 350, 600);
+        Line2D linBOT4 = new Line2D.Float(420, 660, 420, 600);
+        Line2D linBOT5 = new Line2D.Float(490, 660, 490, 600);
+        Line2D linBOT6 = new Line2D.Float(560, 660, 560, 600);
+        Line2D linBOT7 = new Line2D.Float(630, 660, 630, 600);
+        Line2D linBOT8 = new Line2D.Float(700, 660, 700, 600);
+        Line2D linBOT9 = new Line2D.Float(770, 660, 770, 600);
+        g2.draw(linBOT0);
+        g2.draw(linBOT1);
+        g2.draw(linBOT2);
+        g2.draw(linBOT3);
+        g2.draw(linBOT4);
+        g2.draw(linBOT5);
+        g2.draw(linBOT6);
+        g2.draw(linBOT7);
+        g2.draw(linBOT8);
+        g2.draw(linBOT9);
+     
+        
+        Line2D linTOP0 = new Line2D.Float(140, 0, 140, 60);
+        Line2D linTOP1 = new Line2D.Float(210, 0, 210, 60);
+        Line2D linTOP2 = new Line2D.Float(280, 0, 280, 60);
+        Line2D linTOP3 = new Line2D.Float(350, 0, 350, 60);
+        Line2D linTOP4 = new Line2D.Float(420, 0, 420, 60);
+        Line2D linTOP5 = new Line2D.Float(490, 0, 490, 60);
+        Line2D linTOP6 = new Line2D.Float(560, 0, 560, 60);
+        Line2D linTOP7 = new Line2D.Float(630, 0, 630, 60);
+        Line2D linTOP8 = new Line2D.Float(700, 0, 700, 60);
+        Line2D linTOP9 = new Line2D.Float(770, 0, 770, 60);
+        g2.draw(linTOP0);
+        g2.draw(linTOP1);
+        g2.draw(linTOP2);
+        g2.draw(linTOP3);
+        g2.draw(linTOP4);
+        g2.draw(linTOP5);
+        g2.draw(linTOP6);
+        g2.draw(linTOP7);
+        g2.draw(linTOP8);
+        g2.draw(linTOP9);
+        
+        
+        
+        Line2D linRIT0 = new Line2D.Float(700, 600, 770, 600);
+        Line2D linRIT1 = new Line2D.Float(700, 540, 770, 540);
+        Line2D linRIT2 = new Line2D.Float(700, 480, 770, 480);
+        Line2D linRIT3 = new Line2D.Float(700, 420, 770, 420);
+        Line2D linRIT4 = new Line2D.Float(700, 360, 770, 360);
+        Line2D linRIT5 = new Line2D.Float(700, 300, 770, 300);
+        Line2D linRIT6 = new Line2D.Float(700, 240, 770, 240);
+        Line2D linRIT7 = new Line2D.Float(700, 180, 770, 180);
+        Line2D linRIT8 = new Line2D.Float(700, 120, 770, 120);
+        Line2D linRIT9 = new Line2D.Float(700, 60, 770, 60);
+        g2.draw(linRIT0);
+        g2.draw(linRIT1);
+        g2.draw(linRIT2);
+        g2.draw(linRIT3);
+        g2.draw(linRIT4);
+        g2.draw(linRIT5);
+        g2.draw(linRIT6);
+        g2.draw(linRIT7);
+        g2.draw(linRIT8);
+        g2.draw(linRIT9);
+        
+        Line2D linLEFT0 = new Line2D.Float(0, 600, 70, 600);
+        Line2D linLEFT1 = new Line2D.Float(0, 540, 70, 540);
+        Line2D linLEFT2 = new Line2D.Float(0, 480, 70, 480);
+        Line2D linLEFT3 = new Line2D.Float(0, 420, 70, 420);
+        Line2D linLEFT4 = new Line2D.Float(0, 360, 70, 360);
+        Line2D linLEFT5 = new Line2D.Float(0, 300, 70, 300);
+        Line2D linLEFT6 = new Line2D.Float(0, 240, 70, 240);
+        Line2D linLEFT7 = new Line2D.Float(0, 180, 70, 180);
+        Line2D linLEFT8 = new Line2D.Float(0, 120, 70, 120);
+        Line2D linLEFT9 = new Line2D.Float(0, 60, 70, 60);
+        g2.draw(linLEFT0);
+        g2.draw(linLEFT1);
+        g2.draw(linLEFT2);
+        g2.draw(linLEFT3);
+        g2.draw(linLEFT4);
+        g2.draw(linLEFT5);
+        g2.draw(linLEFT6);
+        g2.draw(linLEFT7);
+        g2.draw(linLEFT8);
+        g2.draw(linLEFT9);
+    }
+
+    public static void main(String []args){
+        Board s=new Board();
+        s.setVisible(true);
+    
+       
+    }
+}
---
+++ src/DoubleJeopardy.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+//import all from "awt" and "swing"
+
+public class DoubleJeopardy{
+	public static void main(String[] args){
+		new Board();
+	}
+}
+	
