8fafa875ae4e9e823a52fc7058d4e5bb68ea57f8
jamescrown <jamescrown12345@gmail.com>
2017-02-12 19:59:11 +0000

    Update EnterPlayers.java

--- src/src/EnterPlayers.java
+++ src/src/EnterPlayers.java
@@ -1,11 +1,5 @@
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.Color;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
+import java.awt.*;
+import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
