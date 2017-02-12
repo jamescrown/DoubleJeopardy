5b9115404cca1d7a57d0e6a7c98a5e3fb69727f6
dejirei30 <desiree.pepito@ucdconnect.ie>
2017-02-12 22:45:46 +0000

    Add files via upload

---
+++ InfoPanel.java
@@ -0,0 +1,30 @@
+
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+
+
+public class InfoPanel extends JFrame {
+
+		
+	public InfoPanel() {
+		
+	JTextArea text = new JTextArea(38,20); //  H X WIDTH
+	JScrollPane scroll = new JScrollPane(text, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS); 
+	
+	setLayout(new FlowLayout() );
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	add(scroll);
+	setSize(800,660);
+	setLocationRelativeTo(null);
+	setVisible(true);
+		
+	}
+	
+	public static void main(String args[]) {
+		new InfoPanel();
+	}
+	
+	
+}
