47c5e53eccf74cf75bc0fb79f2cf519f637dc357
dejirei30 <desiree.pepito@ucdconnect.ie>
2017-02-13 09:05:16 +0000

    Update J_SplitPane.java

--- J_SplitPane.java
+++ J_SplitPane.java
@@ -1,90 +1,85 @@
 import java.awt.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class J_SplitPane extends JFrame {
-       	private static final long serialVersionUID = 1L;
-	private JSplitPane splitPaneA;
-	private JSplitPane splitPaneB;
-	private JPanel panel1;
-	private JPanel panel2;
-	private JPanel panel3;
-	private JTextArea textArea;
-	
-
-	/**
-	 * 
-	 */
-	
-
-	public J_SplitPane() {
-		setBackground( Color.gray);
-		
-		 JPanel topPanel = new JPanel();
-		   topPanel.setLayout( new BorderLayout() );
-		   getContentPane().add(topPanel);
-		   
-		   					// Create the panels
-		    create_Panel1();
-		    create_Panel2();
-		    create_Panel3();
-
-		    				// Create a splitter pane
-		    splitPaneA = new JSplitPane( JSplitPane.VERTICAL_SPLIT);
-		    topPanel.add( splitPaneA, BorderLayout.CENTER );
-
-		    splitPaneB = new JSplitPane( JSplitPane.HORIZONTAL_SPLIT );
-		    splitPaneB.setLeftComponent( panel1 );
-		    splitPaneB.setRightComponent( panel2 );
-		    panel2.setLayout(new BorderLayout(0, 0));
-		    {
-		    	textArea = new JTextArea();
-		    	panel2.add(textArea);
-		    }
-
-		    splitPaneA.setLeftComponent( splitPaneB );
-		    splitPaneA.setRightComponent( panel3 );
-	}
-	public void create_Panel1() { // left panel 
-		panel1 = new JPanel();
-		panel1.setLayout(new BorderLayout() );
-		
-		
-	}
-	
-	public void create_Panel2() { // right panel
-		panel2 = new JPanel();
-	
-		
-		
-	}
-	
-	public void create_Panel3() { // bottom panel
-		panel3 = new JPanel();
-		panel3.setLayout(new BorderLayout() );
-		panel3.setPreferredSize( new Dimension( 400, 100 ) );
-	    panel3.setMinimumSize( new Dimension( 100, 50 ) );
-		
-	    panel3.add( new JLabel( "This is the control panel" ), BorderLayout.NORTH );
-	    panel3.add( new JTextArea(), BorderLayout.CENTER );
-	}
-	
-	
-	
-	/**
-	 * 
-	 */
-	
-	public static void main( String args[] ){
-	    try {
-	      
-	    } catch (Exception evt) {}
-	    
-	    // Create an instance of the test application
-	    J_SplitPane mainFrame = new J_SplitPane();
-	    mainFrame.pack();
-	    mainFrame.setVisible( true );
-	}
-
+public class J_SplitPane {
+	 
+    /**
+     * @wbp.parser.entryPoint
+     * 
+     */
+    public static void main(String[] args) {
+    	
+    	new J_SplitPane();
+        SwingUtilities.invokeLater(runJSplitPaneLater);
+    }
+     
+    static Runnable runJSplitPaneLater = new Runnable(){
+ 
+     
+        public void run() 
+        {
+            MyJFrame myJFrame = new MyJFrame();
+            myJFrame.setVisible(true);
+        }
+    };
+     
+    public static class MyJFrame extends JFrame{
+         
+    	
+    	
+        JSplitPane jsp, jsp2;
+        JPanel Panel1, Panel2, Panel3;
+         
+        public MyJFrame()
+        {
+          
+            this.setSize(1000, 800);
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+             
+            
+            JMenuBar menubar = new JMenuBar(); 
+            this.setJMenuBar(menubar);
+            
+            JMenu file = new JMenu("File");
+            menubar.add(file);
+            
+            JMenuItem board = new JMenuItem("open board");
+            file.add(board);
+            
+            Panel1 = new JPanel();
+            Panel2 = new JPanel();
+            Panel3 = new JPanel();
+ 
+           
+            jsp2 = new JSplitPane(JSplitPane.VERTICAL_SPLIT, Panel2, Panel3); 
+            
+            
+            jsp2.setOneTouchExpandable(true);
+            jsp2.setDividerLocation(500);
+             
+            jsp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, 
+                    Panel1,jsp2);
+            
+            jsp.setOneTouchExpandable(true);
+           jsp.setDividerLocation(800);
+             
+            getContentPane().add(jsp);
+       
+           board.addActionListener(new ActionListener()  
+           {
+        	   
+            	public void actionPerformed(ActionEvent e ) 
+            	{
+            		
+            		setVisible(false);
+    				Board board_monopoly = new Board();
+    				board_monopoly.setVisible(true);
+            	}
+           });
+            	
+        }
+    }
 }
