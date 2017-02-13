ff7b63d18e559cf9dc56677bf0b447596c160e1b
jamescrown <jamescrown12345@gmail.com>
2017-02-13 02:53:50 +0000

    Update ButtonFrame2.java

--- src/src/ButtonFrame2.java
+++ src/src/ButtonFrame2.java
@@ -11,7 +11,10 @@ public class ButtonFrame2 extends JFrame{
 	
 	
 	private static final long serialVersionUID = 1L;
-	private static JButton button;
+	public static JButton button;
+	public static JButton button2;
+	public static JButton button3;
+	public static JButton button4;
 	static JLabel label;
 	
 	private static final int FRAME_WIDTH = 100;
@@ -35,19 +38,50 @@ public class ButtonFrame2 extends JFrame{
 			Board.movePieces(n3, n, n2);
 		}
 	}
+	static class PurchaseListener implements ActionListener{
+		public void actionPerformed(ActionEvent event){
+			
+			Random rand = new Random();
+			int  n = rand.nextInt(5) + 1;
+			int n2 = rand.nextInt(5) + 1;
+			int n3 = n + n2;
+			//test
+			//n3 = 12;
+			
+			Board.movePieces(n3, n, n2);
+		}
+	}
 	public static JPanel createComponents() {
+		
+		
 		
 		button = new JButton("Roll Dice!");
 		ActionListener listener = new ClickListener();
 		button.addActionListener(listener);
+	
+		
+		button2 = new JButton("Buy");
+		ActionListener Purchaselistener1 = new PurchaseListener();
+		button2.addActionListener(Purchaselistener1);
 		
 		
-		 
-		JPanel panel = new JPanel();
+		button3 = new JButton("Sell");
+		ActionListener Purchaselistener2 = new PurchaseListener();
+		button3.addActionListener(Purchaselistener2);
+		
+		button4 = new JButton("Trade");
+		ActionListener Purchaselistener3 = new PurchaseListener();
+		button4.addActionListener(Purchaselistener3);
+		
+		JPanel panel1 = new JPanel();
+		//panel1.setLayout(new GridLayout(2, 2));
 //		JLabel lab1 = new JLabel(EnterPlayers.player1 + " Your turn!", JLabel.LEFT);
 //        panel.add(lab1);
-		panel.add(button);
-		return panel;
-		
+		panel1.add(button);
+		panel1.add(button2);
+		panel1.add(button3);
+		panel1.add(button4);
+		return panel1;
+
 	}
 }
